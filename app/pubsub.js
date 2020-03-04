75e25097a3dcf46364cba83f76a4e7f73b939826
dmitryame <dmitry@custommode.com>
2020-03-03 20:47:18 -0500

    debugging peer discovery

--- app/pubsub.js
+++ app/pubsub.js
@@ -40,7 +40,7 @@ class PubSub {
       }
     })
 
-    await node.peerInfo.multiaddrs.add('/ip4/127.0.0.1/tcp/0')
+    await node.peerInfo.multiaddrs.add('/ip4/127.0.0.1/tcp/10335')
     await node.start()
     console.log('libp2p has started')
 
