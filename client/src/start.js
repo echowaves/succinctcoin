2507933f1323bc13a2a238a71725b330fc4fdbb5
dmitryame <dmitry@custommode.com>
2020-06-28 19:59:48 -0400

    wip

--- client/src/start.js
+++ client/src/start.js
@@ -9,29 +9,29 @@ const path = require('path')
 let mainWindow
 
 function createWindow() {
-	mainWindow = new BrowserWindow({ width: 800, height: 600, })
+  mainWindow = new BrowserWindow({ width: 800, height: 600, })
 
-	mainWindow.loadURL(
-		isDev
-			? 'http://localhost:3000'
-			: `file://${path.join(__dirname, '../build/index.html')}`,
-	)
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  )
 
-	mainWindow.on('closed', () => {
-		mainWindow = null
-	})
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
-	if (mainWindow === null) {
-		createWindow()
-	}
+  if (mainWindow === null) {
+    createWindow()
+  }
 })
