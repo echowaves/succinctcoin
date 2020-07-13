5518559a55414a01fbdaa183629e7efeda1cc041
dmitryame <dmitry@custommode.com>
2020-07-12 22:09:47 -0400

    removed catch all raoute in express which was causing warnings anyways

--- src/main/api.js
+++ src/main/api.js
@@ -11,14 +11,14 @@ const TransactionPool = require('./wallet/transaction-pool')
 const Wallet = require('./wallet')
 const TransactionMiner = require('./app/transaction-miner')
 
-const { DEFAULT_PORT, ROOT_NODE_ADDRESS, } = require('../config')
+const { DEFAULT_PORT, ROOT_NODE_ADDRESS } = require('../config')
 
 const app = express()
 const blockchain = new Blockchain()
 const transactionPool = new TransactionPool()
 const wallet = new Wallet()
 
-const pubsub = new PubSub({ blockchain, transactionPool, wallet, })
+const pubsub = new PubSub({ blockchain, transactionPool, wallet })
 
 const transactionMiner = new TransactionMiner({
   blockchain, transactionPool, wallet, pubsub,
@@ -39,8 +39,8 @@ app.get('/api/blocks/length', (req, res) => {
 })
 
 app.get('/api/blocks/:id', (req, res) => {
-  const { id, } = req.params
-  const { length, } = blockchain.chain
+  const { id } = req.params
+  const { length } = blockchain.chain
 
   const blocksReversed = blockchain.chain.slice().reverse()
 
@@ -54,9 +54,9 @@ app.get('/api/blocks/:id', (req, res) => {
 })
 
 app.post('/api/mine', (req, res) => {
-  const { data, } = req.body
+  const { data } = req.body
 
-  blockchain.addBlock({ data, })
+  blockchain.addBlock({ data })
 
   pubsub.broadcastChain()
 
@@ -64,17 +64,17 @@ app.post('/api/mine', (req, res) => {
 })
 
 app.post('/api/transact', (req, res) => {
-  const { amount, recipient, } = req.body
+  const { amount, recipient } = req.body
 
   let transaction = transactionPool
-    .existingTransaction({ inputAddress: wallet.publicKey, })
+    .existingTransaction({ inputAddress: wallet.publicKey })
   try {
     if (transaction) {
       const transactionObj = Object.assign(new Transaction({
         senderWallet: wallet, recipient, amount, outputMap: {}, input: {},
       }), transaction)
 
-      transactionObj.update({ senderWallet: wallet, recipient, amount, })
+      transactionObj.update({ senderWallet: wallet, recipient, amount })
     } else {
       transaction = wallet.createTransaction({
         recipient,
@@ -83,14 +83,14 @@ app.post('/api/transact', (req, res) => {
       })
     }
   } catch (error) {
-    return res.status(400).json({ type: 'error', message: error.message, })
+    return res.status(400).json({ type: 'error', message: error.message })
   }
 
   transactionPool.setTransaction(transaction)
 
   pubsub.broadcastTransaction(transaction)
 
-  res.json({ type: 'success', transaction, })
+  res.json({ type: 'success', transaction })
 })
 
 app.get('/api/transaction-pool-map', (req, res) => {
@@ -108,7 +108,7 @@ app.get('/api/wallet-info', (req, res) => {
 
   res.json({
     address,
-    balance: Wallet.calculateBalance({ chain: blockchain.chain, address, }),
+    balance: Wallet.calculateBalance({ chain: blockchain.chain, address }),
   })
 })
 
@@ -126,12 +126,12 @@ app.get('/api/known-addresses', (req, res) => {
   res.json(Object.keys(addressMap))
 })
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist/index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/index.html'))
+// })
 
 const syncWithRootState = () => {
-  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`, }, (error, response, body) => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const rootChain = JSON.parse(body)
 
@@ -140,7 +140,7 @@ const syncWithRootState = () => {
     }
   })
 
-  request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`, }, (error, response, body) => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const rootTransactionPoolMap = JSON.parse(body)
 
