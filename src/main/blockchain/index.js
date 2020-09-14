3dbd5f540445e70c9956850c30ed7a360a5cb93c
dmitryame <dmitry@custommode.com>
2020-09-14 17:21:18 -0400

    add tests pass

--- src/main/blockchain/index.js
+++ src/main/blockchain/index.js
@@ -1,9 +1,5 @@
 // import Crypto from '../util/crypto'
-import Wallet from './wallet'
-import Transaction from './transaction'
 import Block from './block'
-
-const { REWARD_INPUT, MINING_REWARD } = require('../config')
 
 class Blockchain {
   constructor() {
@@ -18,7 +14,7 @@ class Blockchain {
     this.chain.push(newBlock)
   }
 
-  replaceChain(chain, validateTransactions, onSuccess) {
+  replaceChain(chain, onSuccess) {
     if (chain.length <= this.chain.length) {
       console.error('The incoming chain must be longer') // eslint-disable-line no-console
       return
@@ -29,69 +25,15 @@ class Blockchain {
       return
     }
 
-    if (validateTransactions && !this.validTransactionData({ chain })) {
-      console.error('The incoming chain has invalid data') // eslint-disable-line no-console
-      return
-    }
-
     if (onSuccess) onSuccess()
     console.log('replacing chain with', chain) // eslint-disable-line no-console
     this.chain = chain
-  }
-
-  validTransactionData({ chain }) {
-    for (let i = 1; i < chain.length; i++) { // eslint-disable-line no-plusplus
-      const block = chain[i]
-      const transactionSet = new Set()
-      let rewardTransactionCount = 0
-
-      for (const transaction of block.data) { // eslint-disable-line no-restricted-syntax
-        if (transaction.input.address === REWARD_INPUT.address) {
-          rewardTransactionCount += 1
-
-          if (rewardTransactionCount > 1) {
-            console.error('Miner rewards exceed limit') // eslint-disable-line no-console
-            return false
-          }
-
-          if (Object.values(transaction.outputMap)[0] !== MINING_REWARD) {
-            console.error('Miner reward amount is invalid') // eslint-disable-line no-console
-            return false
-          }
-        } else {
-          if (!Transaction.validTransaction(transaction)) {
-            console.error('Invalid transaction') // eslint-disable-line no-console
-            return false
-          }
-
-          const trueBalance = Wallet.calculateBalance({
-            chain: this.chain,
-            address: transaction.input.address,
-          })
-
-          if (transaction.input.amount !== trueBalance) {
-            console.error('Invalid input amount') // eslint-disable-line no-console
-            console.error(`transaction.input.amount:${transaction.input.amount} trueBalance:${trueBalance}`) // eslint-disable-line no-console
-            return false
-          }
-
-          if (transactionSet.has(transaction)) {
-            console.error('An identical transaction appears more than once in the block') // eslint-disable-line no-console
-            return false
-          }
-          transactionSet.add(transaction)
-        }
-      }
-    }
-
-    return true
   }
 
   static isValidChain(chain) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
       return false
     }
-
     for (let i = 1; i < chain.length; i++) { // eslint-disable-line no-plusplus
       try {
         chain[i].validate()
